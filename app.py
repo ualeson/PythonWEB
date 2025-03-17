19c5738f301858c3671f47164a4ef25953aa7dd1
ualeson <65179704+ualeson@users.noreply.github.com>
2025-03-17 12:50:30 -0300

    atualização do app python e do html

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def template_rendered():
+
+def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
+    
