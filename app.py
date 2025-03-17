d9392f75e4b1f64e434982e4b3c22494395e4b52
ualeson <65179704+ualeson@users.noreply.github.com>
2025-03-17 13:03:33 -0300

    Ajuste do nome da pasta do HTML
    O nome da pasta estava tamplate e precisa estar no plural como tamplates.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return render_template('index.html')
 
+@app.route('/processar', methods=['POST'])
+def processar():
+    nome = request.form['nome']
+    email = request.form['email']
+    return f"<h1>Dados recebidos:</h1><p>Nome: {nome}</p><p>Email: {email}</p>"
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
