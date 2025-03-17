7545e376af24b2dcf0bd14a74108a230d120e503
ualeson <65179704+ualeson@users.noreply.github.com>
2025-03-17 16:31:56 -0300

    Enviando variáveis para o HTML pelo Python

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+nome = "Ualeson"
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', nome=nome)
 
 if __name__ == '__main__':
     app.run(debug=True)
