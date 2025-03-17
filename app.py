ae88617b3da96be100ad1bd44523d9609705f6ef
ualeson <65179704+ualeson@users.noreply.github.com>
2025-03-17 13:04:58 -0300

    Retirei o método processar dados via POST

--- app.py
+++ app.py
@@ -6,11 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/processar', methods=['POST'])
-def processar():
-    nome = request.form['nome']
-    email = request.form['email']
-    return f"<h1>Dados recebidos:</h1><p>Nome: {nome}</p><p>Email: {email}</p>"
-
 if __name__ == '__main__':
     app.run(debug=True)
